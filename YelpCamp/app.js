791a92d361263171797dac02ca6766216bbf3677
Anik <marufmonem@gmail.com>
2020-06-11 17:29:00 +0600

    Yelp camp intial files
    3 pages including forms

---
+++ YelpCamp/app.js
@@ -0,0 +1,55 @@
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine","ejs");
+
+var campgrounds = [
+    {
+        name:"Murgi Vhandar",
+        image: "https://images.unsplash.com/photo-1471115853179-bb1d604434e0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=959&q=80"
+    },
+    {
+        name:"Morog Pahar",
+        image: "https://images.unsplash.com/photo-1492648272180-61e45a8d98a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
+    },
+    {
+        name:"khamar Bari",
+        image: "https://images.unsplash.com/photo-1504280390367-361c6d9f38f4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
+    }
+]
+//root path
+app.get("/", function(req,res){
+    res.render("landing");
+});
+//all campgrounds
+app.get("/campgrounds", function(req,res){
+    res.render("campgrounds",{campgrounds:campgrounds});
+});
+
+//POST route for creating new camp grounds(restful routing)
+app.post("/campgrounds", function(req,res){
+
+    //get data from form and add to campground array
+    var name = req.body.name;
+    var url = req.body.imgUrl;
+    var newCampground = {
+        name:name,
+        image:url
+    }
+    campgrounds.push(newCampground);
+    //redirect back to /campgrounds
+    res.redirect("/campgrounds");
+});
+
+//creating new campgrounds form page
+app.get("/campgrounds/new", function(req,res){
+    res.render("new");
+});
+
+
+
+//connection
+app.listen(5500,"127.0.0.1", function(){
+    console.log("Yelp Camp has started");
+})
