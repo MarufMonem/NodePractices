9e45f7b64d75ffffabe1ae6dd6afce0ad78559f1
Anik <marufmonem@gmail.com>
2020-06-16 12:03:55 +0600

    registering works

--- Authentication/app.js
+++ Authentication/app.js
@@ -12,7 +12,7 @@ mongoose.connect("mongodb://localhost/auth_demo_app" ,{ useNewUrlParser: true })
 
 var app = express();
 app.set("view engine", "ejs");
-
+app.use(bodyParser.urlencoded({extended:true}));
 //complex shortcut method to use a package in the app
 app.use(require("express-session")({
     //secret code to encode and decode the username and password
@@ -31,6 +31,11 @@ app.use(passport.session());
 //the userSchema.
 passport.serializeUser(user.serializeUser()); //encode
 passport.deserializeUser(user.deserializeUser()); //decode
+
+// ************************************
+// ROUTES
+// ************************************
+
 //Root or Index path
 app.get("/", function(req, res){
     res.render("home");
@@ -38,6 +43,43 @@ app.get("/", function(req, res){
 
 app.get("/secret", function(req, res){
     res.render("secret");
+});
+
+//AUTH ROUTES
+
+//NEW
+app.get("/register", function(req, res){
+    res.render("register");
+});
+
+//CREATE
+app.post("/register", function(req, res){
+    user.register(new user({username: req.body.username}), req.body.password, function(err, newUser){
+        if(err){
+            console.log("There is an error: " + err );
+            res.redirect("/register");
+        }else{
+            //once the user is created this would run. 
+            //This logs the user in handles everything related to the session
+            //then run the serialize user method
+            //then specifying that we would use local strategy
+            passport.authenticate("local")(req,res, function(){
+                res.redirect("/secret");
+            });
+        }
+    });
+});
+
+//Login Routes
+
+//render the form NEW
+app.get("/login", function(req, res){
+    res.render("login");
+});
+
+//Create
+app.post("/login", function(req, res){
+    res.render("login");
 });
 
 //connection
