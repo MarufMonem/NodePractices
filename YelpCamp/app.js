fa7ce8660396bcc88dc833c90befcec96e0ca902
Anik <marufmonem@gmail.com>
2020-06-16 17:35:47 +0600

    login feature enabled in yelpcamp

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -1,15 +1,48 @@
 var express         = require("express");
 var app             = express();
 var mongoose        = require("mongoose");
+var localStrategy   = require("passport-local");
+var passport        = require("passport");
+var passportLocalMongoose   = require("passport-local-mongoose");
 var bodyParser      = require("body-parser");
 var campground      = require("./models/campground");
 var comment         = require("./models/comment");
+var user            = require("./models/user");
 var seedDB          = require("./seed");
 mongoose.set('useUnifiedTopology', true); //removing deprication errors
 mongoose.connect("mongodb://localhost/yelp_camp" ,{ useNewUrlParser: true });
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine","ejs");
-app.use(express.static(__dirname + "/public"))
+app.use(express.static(__dirname + "/public"));
+
+//PASSPORT CONFIGURATION
+
+app.use(require("express-session")({
+    //secret code to encode and decode the username and password
+    //can be anything you want
+    secret: "Maruf Monem",
+    resave: false,
+    saveUninitialized:false
+}));
+//These 2 lines are used to make sure our app uses passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+//this line is telling passport to use the user authenticate method
+//which is given to the userSchema
+passport.use(new localStrategy(user.authenticate()))
+//These 2 lines are responsible for reading the session, taking the
+//date from the session encode and decoding it
+//this is able to do this becuase we included passport methods in
+//the userSchema.
+passport.serializeUser(user.serializeUser()); //encode
+passport.deserializeUser(user.deserializeUser()); //decode
+
+//middleware for pasing logged in user information.
+app.use(function(req,res,next){
+    res.locals.currentUser = req.user;
+    next(); // for running the next part of the code
+});
 
 // seedDB();
 //root path
@@ -24,13 +57,13 @@ app.get("/campgrounds", function(req,res){
         if(err){
             console.log(err);
         }else{
-            res.render("campgrounds/index",{campgrounds:allCampground});
+            res.render("campgrounds/index",{campgrounds:allCampground, currentUser:req.user});
         }
     })
 });
 
 //POST route for creating new camp grounds(restful routing CREATE)
-app.post("/campgrounds", function(req,res){
+app.post("/campgrounds", isloggedIn, function(req,res){
 
     //get data from form and add to campground array
     //here we are not using params because the data is not coming from the URL
@@ -56,7 +89,7 @@ app.post("/campgrounds", function(req,res){
 });
 
 //creating new campgrounds form page (NEW)
-app.get("/campgrounds/new", function(req,res){
+app.get("/campgrounds/new",isloggedIn, function(req,res){
     res.render("campgrounds/new");
 });
 
@@ -83,7 +116,7 @@ app.get("/campgrounds/:id", function(req, res){
 //Comment Routes
 
 //Comments Form
-app.get("/campgrounds/:id/comments/new",function(req,res){
+app.get("/campgrounds/:id/comments/new",isloggedIn,function(req,res){
     campground.findById(req.params.id,function(err,foundCamp){
         res.render("comments/new",{camp:foundCamp});
     });
@@ -120,9 +153,66 @@ app.post("/campgrounds/:id/comments",function(req,res){
 
 });
     
+ //***************************************** */ 
+ //Auth Routes
+ 
+ 
+//NEW
+app.get("/register", function(req, res){
+    res.render("register");
+});
+
+//CREATE
+app.post("/register", isloggedIn, function(req, res){
+    user.register(new user({username: req.body.username}), req.body.password, function(err, newUser){
+        if(err){
+            console.log("There is an error: " + err );
+            res.redirect("/register");
+        }else{
+            //once the user is created this would run. 
+            //This logs the user in handles everything related to the session
+            //then run the serialize user method
+            //then specifying that we would use local strategy
+            passport.authenticate("local")(req,res, function(){
+                res.redirect("/campgrounds");
+            });
+        }
+    });
+});
+
+//Login Routes
+
+//render the form NEW
+app.get("/login", function(req, res){
+    res.render("login");
+});
+
+//Create
+//passport authenticate middleware
+app.post("/login", passport.authenticate("local", {
+    successRedirect: "/campgrounds", //if the login was successful
+    failureRedirect: "/login" // if it wasnt
+}) , function(req, res){
     
+});
+
+//logging out handler
+app.get("/logout", function(req, res){
+    req.logOut();
+    res.redirect("/campgrounds");
+});
+
+//logged in checker
+function isloggedIn(req,res,next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
 
 
+
+ //***************************************** */
 
 //connection
 app.listen(5500,"127.0.0.1", function(){
