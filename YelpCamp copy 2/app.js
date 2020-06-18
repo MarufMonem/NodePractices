6469d55f9b68f3231ee58a79a25c25514347c676
Anik <marufmonem@gmail.com>
2020-06-18 17:29:28 +0600

    New version of yelpcamp that has full CRUD

---
+++ YelpCamp copy 2/app.js
@@ -0,0 +1,60 @@
+var express         = require("express");
+var app             = express();
+var mongoose        = require("mongoose");
+var localStrategy   = require("passport-local");
+var passport        = require("passport");
+var passportLocalMongoose   = require("passport-local-mongoose");
+var bodyParser      = require("body-parser");
+var campground      = require("./models/campground");
+var comment         = require("./models/comment");
+var user            = require("./models/user");
+var seedDB          = require("./seed");
+
+var commentRoutes       = require("./routes/comments"),
+    campgroundRoutes    = require("./routes/campgrounds"),
+    indexRoutes         = require("./routes/index");
+mongoose.set('useUnifiedTopology', true); //removing deprication errors
+mongoose.connect("mongodb://localhost/yelp_camp" ,{ useNewUrlParser: true });
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine","ejs");
+app.use(express.static(__dirname + "/public"));
+
+//PASSPORT CONFIGURATION
+
+app.use(require("express-session")({
+    //secret code to encode and decode the username and password
+    //can be anything you want
+    secret: "Maruf Monem",
+    resave: false,
+    saveUninitialized:false
+}));
+//These 2 lines are used to make sure our app uses passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+//this line is telling passport to use the user authenticate method
+//which is given to the userSchema
+passport.use(new localStrategy(user.authenticate()))
+//These 2 lines are responsible for reading the session, taking the
+//date from the session encode and decoding it
+//this is able to do this becuase we included passport methods in
+//the userSchema.
+passport.serializeUser(user.serializeUser()); //encode
+passport.deserializeUser(user.deserializeUser()); //decode
+
+//middleware for pasing logged in user information.
+app.use(function(req,res,next){
+    res.locals.currentUser = req.user;
+    next(); // for running the next part of the code
+});
+
+// seedDB();
+
+app.use("/",indexRoutes); //add / means its the same location
+app.use("/campgrounds/:id/comments",commentRoutes); //what this is doing is that all campground routes append this /campground before there own routes
+app.use("/campgrounds",campgroundRoutes); //what this is doing is that all campground routes append this /campground before there own routes
+
+//connection
+app.listen(5500,"127.0.0.1", function(){
+    console.log("Yelp Camp has started");
+})
---
+++ YelpCamp copy 2/routes/comments.js
@@ -0,0 +1,57 @@
+var express = require("express");
+var router = express.Router({mergeParams:true});
+var campground = require("../models/campground");
+var comment = require("../models/comment");
+//Comment Routes
+
+//Comments Form
+router.get("/new",isloggedIn,function(req,res){
+    campground.findById(req.params.id,function(err,foundCamp){
+        res.render("comments/new",{camp:foundCamp});
+    });
+
+});
+
+//Comments create logic handler
+router.post("/",isloggedIn,function(req,res){
+    campground.findById(req.params.id,function(err,foundCamp){
+        console.log("***************comments***********************");
+        console.log("THE FOUND CAMP: " + foundCamp);
+        console.log("===============comments=======================");
+        if(err){
+            console.log("ERROR :" + err);
+            res.redirect("/campgrounds/"+ req.params.id);
+        }else{
+            comment.create(req.body.comment, function(err, newComment){
+                if(err){
+                    console.log(err);
+                }else{
+                    newComment.author.id = req.user._id;
+                    newComment.author.username = req.user.username;
+                    newComment.save();
+                    foundCamp.comments.push(newComment);
+                    foundCamp.save(function(err, savedCampground){
+                        if(err){
+                            console.log("Couldnt add the comment");
+                        }else{
+                            res.redirect("/campgrounds/"+ req.params.id);
+                        }
+                    });
+                }
+            });
+        }
+
+    });
+
+});
+
+//logged in checker
+function isloggedIn(req,res,next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
+
+
+module.exports= router;
